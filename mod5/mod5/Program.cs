cdb711d09c4efaa10e8e8b528fb355d4d6eb3766
Aaron Evanovich <A.Evanovich@outlook.com>
2017-02-10 17:17:41 +0800

    before checking against answer

--- mod5/mod5/Program.cs
+++ mod5/mod5/Program.cs
@@ -10,47 +10,172 @@ namespace mod5
     {
         static void Main(string[] args)
         {
-            int nameList;
-            Console.WriteLine("How many names are you going to put in?");
-            nameList = Convert.ToInt32(Console.ReadLine());
+            Student s1 = new Student("John", "Smith", "M");
+            Student s2 = new Student("Tom", "Orr", "M");
+            Student s3 = new Student("Ellen", "Tousi", "F");
 
-            DrinksMachine[] dm = new DrinksMachine[nameList];
+            Course c1 = new Course("Programming with C#", 3);
+            c1._s[0] = s1;
+            c1._s[1] = s2;
+            c1._s[2] = s3;
+            Teacher t1 = new Teacher("Walter", "White", "M");
+            c1.Staff = t1;
+            Degree d1 = new Degree("Bachelor");
+            d1.Cs = c1;
+            UProgram u1 = new UProgram("Information Technology");
+            u1.Dg = d1;
 
-            //DrinksMachine tm = new DrinksMachine();
-            //DrinksMachine jn = new DrinksMachine("John");
-
-            for (int i = 0; i < nameList; i++)
-            {
-                Console.WriteLine("The detail of the {0} customer is?", i);
-                dm[i] = new DrinksMachine(Console.ReadLine());
-            }
-
-            for (int j = 0; j < nameList; j++)
-            {
-                Console.WriteLine("The name of the {0} customer is {1}.", j, dm[j].customer);
-            }
-
+            Console.WriteLine("{0} program contains {1} degree.", u1.Name, u1.Dg.Name);
+            Console.WriteLine("{0} degree contains {1} course.", d1.Name, d1.Cs.Name);
+            Console.WriteLine("{0} course contains {1} students.", c1.Name, Student.StudentCount());
             Console.ReadLine();
+
         }
     }
-    class DrinksMachine
+    class Student
     {
-        public string customer { get; set; }
-        public string drinks { get; set; }
-        public int number { get; set; }
-        //constructor
-        internal DrinksMachine(string customer = "Tom", string drinks = "coffee", int number = 1)
+        private static int _studentCount;
+        private string _firstName;
+        private string _lastName;
+        private string _sex;
+        
+        #region Properties
+        public string FirstName
         {
-            this.customer = customer;
-            this.drinks = drinks;
-            this.number = number;
+            get { return _firstName; }
+            set { _firstName = value; }
         }
-        ///method
-        internal Boolean namelen(string name)
+        public string LastName
         {
-            if (name.Length > 4) return true;
-            else return false;
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+        public string Sex
+        {
+            get { return _sex; }
+            set { _sex = value; }
+        }
+        #endregion
+
+        public Student()
+        {
+            _studentCount++;
+        }
+        public Student(string first, string last, string sex)
+        {
+            FirstName = first;
+            LastName = last;
+            Sex = sex;
+            _studentCount++;
         }
 
+        public static int StudentCount()
+        {
+            return _studentCount;
+        }
+    }
+    class Teacher
+    {
+        private string _firstName;
+        private string _lastName;
+        private string _sex;
+
+        #region Properties
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+        public string Sex
+        {
+            get { return _sex; }
+            set { _sex = value; }
+        }
+        #endregion
+
+        public Teacher(string first, string last, string sex)
+        {
+            FirstName = first;
+            LastName = last;
+            Sex = sex;
+        }
+    }
+    class Course
+    {
+        private string _name;
+        private int _credit;
+        public Student[] _s = new Student[3];
+        private Teacher _t;
+        
+
+        #region Properties
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public int Credit
+        {
+            get { return _credit; }
+            set { _credit = value; }
+        }
+        public Teacher Staff
+        {
+            get { return _t; }
+            set { _t = value; }
+        }
+        #endregion
+
+        public Course(string name, int credit)
+        {
+            Name = name;
+            Credit = credit;
+        }        
+    }
+    class Degree
+    {
+        private string _name;
+        private Course _cs;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public Course Cs
+        {
+            get { return _cs; }
+            set { _cs = value; }
+        }
+        public Degree(string name)
+        {
+            Name = name;
+        }
+
+    }
+    class UProgram
+    {
+        private string _name;
+        private Degree _dg;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public Degree Dg
+        {
+            get { return _dg; }
+            set { _dg = value; }
+        }
+        public UProgram(string name)
+        {
+            Name = name;
+        }
     }
 }
